db6b7f35bfd6b7f7adf6fc4d4bab141249c4adc1
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-16 20:54:11 +0800

    Installed React Router from the npm registry with npm

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -5,9 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Fligno Take-Home Exam</title>
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         
-        
-
     </head>
     <body>
         <div id="example">
