3abe962448bef9864be92d493066bf82414c297c
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-17 11:53:38 +0800

    Added Post API

--- example-app/app/Http/Controllers/HomeController.php
+++ example-app/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\Post as PostResource;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return  PostResource::collection(Post::all());
     }
 }
