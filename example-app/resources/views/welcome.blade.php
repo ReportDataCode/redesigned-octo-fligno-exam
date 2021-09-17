a606c99c7ac83a31a5abc464f1fe7986ffa9e2e0
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-17 10:46:14 +0800

    "Added Post.PHP

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -4,14 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Fligno Take-Home Exam </title>
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <title>Laravel App </title>
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}" />
         
     </head>
     <body>
         <div id="app">
-    
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script  defer src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
